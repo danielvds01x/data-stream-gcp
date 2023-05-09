6ec12a65d100601fa0947c7d9d23ea838d4d91cb
Daniel Vieira <daniel@gmx.com>
2023-05-09 15:08:12 -0300

    chore: disable terraform destroy command

--- cleanup.sh
+++ cleanup.sh
@@ -3,21 +3,33 @@
 DOCKER_IMAGE='iac'
 
 # Destroy the Terraform resources and auto-approve
-docker run -i \
--v $HOME/.config/gcloud:/root/.config/gcloud \
--t $DOCKER_IMAGE destroy -auto-approve
+# docker run -i \
+# -v $HOME/.config/gcloud:/root/.config/gcloud \
+# -t $DOCKER_IMAGE destroy -auto-approve
+
+# docker run -i \
+# -v $HOME/.config/gcloud:/root/.config/gcloud \
+# -t $DOCKER_IMAGE plan
+
+# # docker exec -it $(docker ps -q --filter "ancestor=iac") terraform plan
+# docker exec -it $(docker ps -q --filter "ancestor=iac") terraform apply -auto-approve
+# docker exec -it $(docker ps -q --filter "ancestor=iac") terraform detsroy -auto-approve
+
 
 # Remove any running Docker containers with the "pub-app-emulator" image
-# docker stop $(docker ps -q --filter "ancestor=iac")
 docker ps --filter "ancestor=iac" -q | xargs -r docker stop -f
+
 # Delete all stopped containers
 docker container prune -f
+
 # Remove any Docker images with the "pub-app-emulator" tag
 docker images --filter "reference=iac" -q | xargs -r docker rmi
 
 # Remove any running Docker containers with the "pub-app-emulator" image
 docker ps --filter "ancestor=pub-app-emulator" -q | xargs -r docker stop
+
 # Delete all stopped containers
 docker container prune -f
+
 # Remove any Docker images with the "pub-app-emulator" tag
 docker images --filter "reference=pub-app-emulator" -q | xargs -r docker rmi
